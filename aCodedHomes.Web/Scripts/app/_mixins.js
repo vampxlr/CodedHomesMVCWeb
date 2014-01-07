dd2107617f11a932a20e31a657c2a5f5cfdc31e4
Ahsan <vampxlr@yahoo.com>
2014-01-08 04:53:23 +0600

    removed

--- aCodedHomes.Web/Scripts/app/_mixins.js
+++
@@ -1,7 +0,0 @@
-﻿var _mixIns = (function() {
-    _.mixin({
-        numberWithCommas: function(value) {
-            return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-        }
-    });
-}());
